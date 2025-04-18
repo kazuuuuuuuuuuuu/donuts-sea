8e6b15a9247e8339afcc084fa950c68c7e2b87a9
kazuuuuuuuuuuuu <li-278139@hotmail.com>
2025-04-18 15:00:18 +0800

    change some text

--- src/script.js
+++ src/script.js
@@ -37,7 +37,7 @@ fontLoader.load(
 
         // Text
         const textGeometry = new TextGeometry(
-            'Hello Three.js',
+            'Kazu coming through',
             {
                 font: font,
                 size: 0.5,
